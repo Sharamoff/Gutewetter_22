eb73e01e1ab3f63f2a6fddd2e67208d871d42acc
Ihar Sharamau <sharamoff@gmail.com>
2023-01-10 15:40:27 +0100

    Productlist page v.1

--- _PROJECT/SRC/js/vendor.js
+++ _PROJECT/SRC/js/vendor.js
@@ -1,10 +1,11 @@
 /* vendor */
 
 //= vendor/jquery-3.6.1.min.js
+//= vendor/jquery-ui.min.js
+//= vendor/jquery.ui.touch-punch.min.js
 
 //= vendor/bootstrap.bundle.min.js
 
 //= vendor/slick.min.js
 
-//= vendor/hc-sticky.js
 
