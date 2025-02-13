14bb218bd6e4e1920167087687717c3a6e66ff66
Ihar Sharamau <sharamoff@gmail.com>
2025-02-13 12:05:24 +0100

    Project Collector Upgrade (package.json + gulpfile.js)

--- _PROJECT/gulpfile.js
+++ _PROJECT/gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
 
+const sass = require('gulp-sass')(require('sass'));
+
 const
 	gulp = require('gulp'),
-	sass = require('gulp-sass')(require('sass')),
 	plumber = require('gulp-plumber'),
 	prefixer = require('gulp-autoprefixer'),
 	rigger = require('gulp-rigger'),
@@ -21,24 +22,24 @@ const path = {
 	},
 	src: {
 		html:   'SRC/*.html',
-		js:     'SRC/js/*.*js',
+		js:     'SRC/js/*.js',
 		style:  'SRC/style/*.scss',
 		img:    'SRC/img/**/*.*',
 		fonts:  'SRC/fonts/**/*.*'
 	},
 	watch: {
 		html:   'SRC/**/*.html',
-		js:     'SRC/js/**/*.*js',
+		js:     'SRC/js/**/*.js',
 		style:  'SRC/style/**/*.scss',
 		img:    'SRC/img/**/*.*',
 		fonts:  'SRC/fonts/**/*.*'
 	},
-	clean: './build'
+	clean: './BUILD'
 };
 
 const config = {
 	server: {
-		baseDir: "./build"
+		baseDir: "./BUILD"
 	},
 	tunnel: false,
 	host: 'localhost',
@@ -46,23 +47,21 @@ const config = {
 	logPrefix: "Promoting"
 };
 
-gulp.task('html:build', function(done) {
+gulp.task('html:build', () => {
 	return gulp.src(path.src.html)
 		.pipe(rigger())
 		.pipe(gulp.dest(path.build.html))
 		.pipe(reload({stream: true}));
-	done();
 });
 
-gulp.task('js:build', function(done) {
+gulp.task('js:build', () => {
 	return gulp.src(path.src.js)
 		.pipe(rigger())
 		.pipe(gulp.dest(path.build.js))
 		.pipe(reload({stream: true}));
-	done();
 });
 
-gulp.task('style:build', function(done) {
+gulp.task('style:build', () => {
 	return gulp.src(path.src.style)
 		.pipe(plumber())
 		.pipe(sass({
@@ -72,51 +71,46 @@ gulp.task('style:build', function(done) {
 		.pipe(prefixer())
 		.pipe(gulp.dest(path.build.css))
 		.pipe(reload({stream: true}));
-	done();
 });
 
-gulp.task('image:build', function(done) {
+gulp.task('image:build', () => {
 	return gulp.src(path.src.img)
 		.pipe(gulp.dest(path.build.img))
 		.pipe(reload({stream: true}));
-	done();
 });
 
-gulp.task('fonts:build', function(done) {
+gulp.task('fonts:build', () => {
 	return gulp.src(path.src.fonts)
 		.pipe(gulp.dest(path.build.fonts));
-	done();
 });
 
 gulp.task('build', gulp.parallel(
-	'html:build',
-	'js:build',
-	'style:build',
-	'fonts:build',
-	'image:build'
+		'html:build',
+		'js:build',
+		'style:build',
+		'fonts:build',
+		'image:build'
 	)
 );
 
 
-gulp.task('watch', function(done) {
+gulp.task('watch', () => {
 	gulp.watch(path.watch.html,  gulp.series('html:build'));
 	gulp.watch(path.watch.js,    gulp.series('js:build'));
 	gulp.watch(path.watch.style, gulp.series('style:build'));
 	gulp.watch(path.watch.fonts, gulp.series('fonts:build'));
-	gulp.watch(path.watch.img, gulp.series('image:build'));
-	done();
+	gulp.watch(path.watch.img,   gulp.series('image:build'));
 });
 
 
-gulp.task('webserver', function(done) {
+gulp.task('webserver', () => {
 	browserSync.init(config);
-	done();
 });
 
 
-gulp.task('clean', function(cb) {
+gulp.task('clean', (cb) => {
 	rimraf(path.clean, cb);
 });
 
 
-gulp.task('default', gulp.series('build', 'webserver', 'watch'));
+gulp.task('default', gulp.series('build', gulp.parallel('webserver', 'watch')));
